46f63c958ae9f83bd199ebeee25a98eb2255cd78
Your Name <you@example.com>
2015-11-17 19:01:52 +0000

    Using anonymous functions

--- js/blacklist.js
+++ js/blacklist.js
@@ -1,9 +1,38 @@
+(function () {
 
+    function blacklist() {
+        this._started = false;
+        uploader.prototype.start();
 
-window.addEventListener('DOMContentLoaded', function() {
+    }
+    blacklist.prototype = {
+        /**
+         * Start to handle screenshot events.
+         * @memberof Screenshot.prototype
+         */
+        start: function () {
+            console.log("Blacklist: Ready to load");
+            if (document.documentElement) {
+                this._started = true;
+                console.log("Blacklist: loaded.");
+            } else {
+                this._started = true;
+                console.log("Blacklist: loaded.");
+            }
+        },
+        /**
+         * Stop handling screenshot events.
+         * @memberof Screenshot.prototype
+         */
+        stop: function () {
+            console.log("FXOS-Adblocker Stopped at: " + window.location);
+            if (!this._started) {
+                throw 'Blacklist was never start()\'ed but stop() is called.';
+            }
+            this._started = false;
+        }
 
-  console.log("blacklist addon dificil started");
+    }
 
-
-
-});
+    var blacklist = new blacklist();
+}());
